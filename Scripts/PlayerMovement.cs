0d0f5474941f2f78f9c9f6882988039f89feba7e
GIuliannom <giulianno.gasparato@knights.ucf.edu>
2023-07-07 14:36:31 -0400

    A
    A

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    [SerializeField] private float moveSpeed = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        float dirY = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(dirX * moveSpeed, dirY * moveSpeed);
+    }
+}
